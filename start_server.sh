0ba7dc7a2f1743711d6fa913bf1e686bf47e9186
Kartik <kartik.pawade.work@gmail.com>
2026-03-02 21:23:06 +0530

    feat:Structured logging using structlog for main components

--- start_server.sh
+++ start_server.sh
@@ -6,8 +6,6 @@ cd "$SCRIPT_DIR"
 
 echo "Starting the API server (Uvicorn)..."
 
-# ! In case you are not using Poetry
-# uvicorn src.server:app --reload --host 0.0.0.0 --port 8000
 
 # ! In case you are using Poetry 
 poetry run uvicorn src.server:app --reload --host 0.0.0.0 --port 8000
